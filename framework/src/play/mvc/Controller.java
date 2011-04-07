dc1f05f9a5eb11f37675993a2a6125c875a4b274
greenlaw110 <greenlaw110@gmail.com>
2011-04-08 09:13:53 +1000

    register/unregister json serializer registry

--- framework/src/play/mvc/Controller.java
+++ framework/src/play/mvc/Controller.java
@@ -345,6 +345,19 @@ public class Controller implements ControllerSupport, LocalVariablesSupport {
     }
     
     /**
+     * Global JsonSerializer Registry
+     */
+    private static Map<Class<?>, JsonSerializer<?>> jsonSerializers = new HashMap<Class<?>, JsonSerializer<?>>();
+    
+    protected static void registerJsonSerializer(JsonSerializer<?> jsonSerializer) {
+        jsonSerializers.put(jsonSerializer.getClass(), jsonSerializer);
+    }
+    
+    protected static void unregisterJsonSerializer(JsonSerializer<?> jsonSerializer) {
+        jsonSerializers.remove(jsonSerializer.getClass());
+    }
+    
+    /**
      * Render a 200 OK application/json response
      * @param args The Java objects and a set of GSON serializers/deseralizers/instance creator to use
      */
@@ -361,6 +374,7 @@ public class Controller implements ControllerSupport, LocalVariablesSupport {
                 templateBinding.put(name, o);
             }
         }
+        l.addAll(jsonSerializers.values());
         throw new RenderJson(templateBinding, l.toArray(new JsonSerializer[]{}));
     }
 
