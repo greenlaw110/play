e895df01e5c92a13e1b8aca8d08f1f77527a3d58
greenlaw110 <greenlaw110@gmail.com>
2011-05-25 11:30:16 +1000

    strip out Play.ctxPath when parsing request

--- framework/src/play/server/PlayHandler.java
+++ framework/src/play/server/PlayHandler.java
@@ -481,6 +481,7 @@ public class PlayHandler extends SimpleChannelUpstreamHandler {
             path = URLDecoder.decode(uri.substring(0, i), encoding);
             querystring = uri.substring(i + 1);
         }
+        path = path.replaceFirst(Play.ctxPath, "");
 
         String remoteAddress = getRemoteIPAddress(ctx);
         String method = nettyRequest.getMethod().getName();
